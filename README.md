18d45269c88c03308a59800cc4658efb2742dc26
Emil Madsen <sovende@gmail.com>
2021-07-27 14:23:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<!--
+SPDX-FileCopyrightText: Magenta ApS
+
+SPDX-License-Identifier: MPL-2.0
+-->
+
+# OS2mo AMQP Trigger Example
+
+This repository contains an example implementation of an OS2mo AMQP Trigger receiver.
+
+## Usage
+Start the container using `docker-compose`:
+```
+docker-compose up -d
+```
---
+++ main.py
@@ -0,0 +1,96 @@
+import asyncio
+import json
+from typing import List
+from uuid import uuid4
+
+import click
+from aio_pika import connect
+from aio_pika import ExchangeType
+from aio_pika import IncomingMessage
+
+
+def on_message(message: IncomingMessage) -> None:
+    with message.process():
+        print(json.dumps({
+            "routing-key": message.routing_key,
+            "body": json.loads(message.body)
+        }, indent=4))
+
+
+async def main(
+    host: str,
+    port: int,
+    username: str,
+    password: str,
+    exchange: str,
+    routing_key: List[str],
+) -> None:
+    print(f"Establishing AMQP connection to amqp://{username}:xxxxx@{host}:{port}/")
+    connection = await connect(f"amqp://{username}:{password}@{host}:{port}/")
+
+    print("Creating AMQP channel")
+    channel = await connection.channel()
+    await channel.set_qos(prefetch_count=1)
+
+    print("Attaching AMQP exchange to channel")
+    topic_logs_exchange = await channel.declare_exchange(exchange, ExchangeType.TOPIC)
+
+    # Declaring queue
+    queue_name = 'os2mo-consumer-' + str(uuid4())
+    print(f"Declaring unique message queue: {queue_name}")
+    queue = await channel.declare_queue(queue_name, durable=False)
+
+    for key in routing_key:
+        print(f"Binding routing-key: {key}")
+        await queue.bind(topic_logs_exchange, routing_key=key)
+
+    print("Listening for messages")
+    await queue.consume(on_message)
+
+
+@click.command()
+@click.option(
+    "--host",
+    default="localhost",
+    help="AMQP host",
+    show_default=True,
+)
+@click.option(
+    "--port",
+    type=click.INT,
+    default=5672,
+    help="AMQP port",
+    show_default=True,
+)
+@click.option(
+    "--username",
+    default="guest",
+    help="AMQP username",
+    show_default=True,
+)
+@click.option(
+    "--password",
+    prompt=True,
+    hide_input=True,
+    required=True,
+    help="AMQP password",
+)
+@click.option(
+    "--exchange",
+    default="os2mo",
+    help="AMQP exchange",
+    show_default=True,
+)
+@click.option(
+    "--routing-key",
+    help="AMQP routing keys",
+    multiple=True,
+)
+def cli(**kwargs):
+    loop = asyncio.get_event_loop()
+    loop.create_task(main(**kwargs))
+    loop.run_forever()
+
+
+if __name__ == "__main__":
+    cli(auto_envvar_prefix="AMQP")
