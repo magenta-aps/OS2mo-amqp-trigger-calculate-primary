954f3c17cd9ba5fd529eda7581719d94d41c755a
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2024-08-02 10:55:50 +0200

    refactor: put relevant settings into config.py

---
+++ calculate_primary/config.py
@@ -0,0 +1,18 @@
+
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+from typing import Literal
+from uuid import UUID
+
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    class Config:
+        frozen = True
+        env_nested_delimiter = "__"
+    
+    mo_url: str
+
+    amqp_integration: Literal["DEFAULT", "OPUS", "SD"]
+    dry_run: bool = False
+    eng_types_primary_order: list[UUID] = []
